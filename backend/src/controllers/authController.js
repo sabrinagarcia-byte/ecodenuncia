df368015b3ff8f4c70ef6f15ec174816f853ea05
acomnene-source <a.comnene@aluno.ifsp.edu.br>
2026-04-27 17:28:32 -0300

    adicionado mock api

---
+++ backend/src/controllers/authController.js
@@ -0,0 +1,3 @@
+import usuario from "../data/mock.js"
+
+console.log(usuario)
---
+++ backend/src/data/mock.js
@@ -0,0 +1,16 @@
+const usuario = [
+    {
+        id: 1,
+        nome: "Ana",
+        email: "ana@email.com",
+        senha: "123456"
+    },
+    {
+        id: 2,
+        nome: "João",
+        email: "joao@email.com",
+        senha: "abcdef"
+    }
+];
+
+export default usuario
